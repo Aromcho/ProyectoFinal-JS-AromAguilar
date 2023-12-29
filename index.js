46d10e36e134d73c3e4ff8f61c73ff947cf42805
Arom Aguilar <barriosarom@gmail.com>
2023-12-29 16:01:40 -0300

    Mensaje descriptivo del commit

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ const productosArray = [
   {
     id: "Duke-390",
     titulo: "Duke 390",
+    ficha: `<div class="capa">
+    <div class="detalles"><h6>Cilindrada:</h6><p>249 cc</p></div>
+    <div class="detalles"><h6>Velocidad máxima:</h6><p>140 km/h</p></div>
+    <div class="detalles"><h6>Alimentación:</h6><p>Inyección</p></div>
+    <div class="detalles"><h6>Transmisión:</h6><p>6 velocidades</p></div>
+  </div>`,
     imagen: "./img/duke 390.webp",
     categoria: {
       nombre: "KTM",
@@ -33,7 +39,13 @@ const productosArray = [
   },
   {
     id: "fz-150",
-    titulo: "fz 150",
+    titulo: "FZ 150",
+    ficha: `<div class="capa">
+    <div class="detalles"><h6>Cilindrada:</h6><p>149 cc</p></div>
+    <div class="detalles"><h6>Velocidad máxima:</h6><p>115 km/h</p></div>
+    <div class="detalles"><h6>Alimentación:</h6><p>Inyección</p></div>
+    <div class="detalles"><h6>Transmisión:</h6><p>5 velocidades</p></div>
+  </div>`,
     imagen: "./img/fz 150.webp",
     categoria: {
       nombre: "Yamaha",
@@ -43,7 +55,13 @@ const productosArray = [
   },
   {
     id: "ninja-400",
-    titulo: "ninja 400",
+    ficha: `<div class="capa">
+    <div class="detalles"><h6>Cilindrada:</h6><p>399 cc</p></div>
+    <div class="detalles"><h6>Velocidad máxima:</h6><p>170 km/h</p></div>
+    <div class="detalles"><h6>Alimentación:</h6><p>Inyección</p></div>
+    <div class="detalles"><h6>Transmisión:</h6><p>6 velocidades</p></div>
+  </div>`,
+    titulo: "Ninja 400",
     imagen: "./img/ninja 400.webp",
     categoria: {
       nombre: "Kawasaki",
@@ -53,24 +71,53 @@ const productosArray = [
   },
   {
     id: "gn-125",
-    titulo: "gn 125",
+    titulo: "GN 125",
+    ficha: `<div class="capa">
+    <div class="detalles"><h6>Cilindrada:</h6><p>124 cc</p></div>
+    <div class="detalles"><h6>Velocidad máxima:</h6><p>105 km/h</p></div>
+    <div class="detalles"><h6>Alimentación:</h6><p>carburador</p></div>
+    <div class="detalles"><h6>Transmisión:</h6><p>5 velocidades</p></div>
+  </div>`,
     imagen: "./img/suzuki-gn-125.jpg",
     categoria: {
       nombre: "Suzuki",
       id: "suzuki"
     },
-    precio: 13350
+    precio: 1900
   },
   { 
     id: "NC750X",
     titulo: "NC750X",
+    ficha: `<div class="capa">
+    <div class="detalles"><h6>Cilindrada:</h6><p>745 cc</p></div>
+    <div class="detalles"><h6>Velocidad máxima:</h6><p>180 Km/h</p></div>
+    <div class="detalles"><h6>Alimentación:</h6><p>Inyección</p></div>
+    <div class="detalles"><h6>Transmisión:</h6><p>6 velocidades</p></div>
+  </div>`,
     imagen: "./img/NC750X.webp",
     categoria: {
       nombre: "Honda",
       id: "honda"
     },
-    precio: 13350
-  }
+    precio: 15500
+  },
+  { 
+    id: "SMX-200",
+    titulo: "SMX 200 Adventure",
+    ficha: `<div class="capa">
+    <div class="detalles"><h6>Cilindrada:</h6><p>205cc</p></div>
+    <div class="detalles"><h6>Velocidad máxima:</h6><p>105 km/h</p></div>
+    <div class="detalles"><h6>Alimentación:</h6><p>carburador</p></div>
+    <div class="detalles"><h6>Transmisión:</h6><p> velocidades</p></div>
+  </div>`,
+    imagen: "./img/smx-200-adv.jpeg",
+    categoria: {
+      nombre: "Gilera",
+      id: "gilera"
+    },
+    precio: 1680
+  },
+
 ];
 
 const contenedorProductos = document.querySelector("#contenedor-productos");
@@ -81,6 +128,8 @@ const productosEnCarrito = [];
 
 function limpiarContenedorProductos() {
   contenedorProductos.innerHTML = "";
+
+
 }
 
 function cargarProductos(productos) {
@@ -91,10 +140,7 @@ function cargarProductos(productos) {
     div.innerHTML = `
       <figure>
         <img src="${producto.imagen}" class="card-img-top" alt="${producto.titulo}">
-        <div class="capa">
-          <h3>aqui va la ficha técnica</h3>
-          <p>aqui va otra cosa</p>
-        </div>
+        ${producto.ficha}
       </figure>
       <div class="card-body">
         <h5 class="card-title">${producto.titulo}</h5>
@@ -104,12 +150,15 @@ function cargarProductos(productos) {
       </div>
     </div> `;
     contenedorProductos.append(div);
+    
   });
 }
 
 function agregarAlCarrito(productoId) {
   const producto = productosArray.find(p => p.id === productoId);
-  productosEnCarrito.push(producto);
+  const productoEnCarrito = { ...producto, precioARS: convertirPrecio(producto.precio) };
+  productosEnCarrito.push(productoEnCarrito);
+  actualizarCarrito();
   localStorage.setItem('productosEnCarrito', JSON.stringify(productosEnCarrito));
   Swal.fire({
     position: "top-end",
@@ -119,11 +168,16 @@ function agregarAlCarrito(productoId) {
     timer: 1500
   });
 }
-
 function cargarProductosDesdeLocalStorage() {
   const productosGuardados = JSON.parse(localStorage.getItem('productosEnCarrito')) || [];
-  productosEnCarrito.push(...productosGuardados);
+  productosEnCarrito.length = 0;
+  productosGuardados.forEach(producto => {
+    producto.precioARS = convertirPrecio(producto.precio);
+    productosEnCarrito.push(producto);
+  });
+  actualizarPreciosCarrito();
 }
+
 
 function eliminarDelCarrito(productoIndex) {
   productosEnCarrito.splice(productoIndex, 1);
@@ -146,7 +200,7 @@ function actualizarCarrito() {
           <div class="cont-producto-carrito">
               <img src="${producto.imagen}" class="foto-carrito" alt="${producto.titulo}"> 
               <p class="nombre-producto">${producto.titulo}</p>
-              <p class="card-text precio">U$S${producto.precio}</p>
+              <p class="card-text precio">ARS $ ${producto.precioARS}</p>
               <button class="papelera">borrar</i></button>
           </div>
       </div>`).join('');
@@ -155,13 +209,14 @@ function actualizarCarrito() {
       const barraInferiorHTML = productosEnCarrito.length > 0 ? `
         <div class="barra-inferior-carrito">
           <p>Total:</p>
-          <p>U$S${total}</p>
+          <p>ARS $ ${total}</p>
           <button id="" class="btn btn-primary">Finalizar compra</button>
         </div>` : '';
     
       carrito.innerHTML = productosEnCarritoHTML + barraInferiorHTML;
-    
+      
       localStorage.setItem('productosEnCarrito', JSON.stringify(productosEnCarrito));
+      
 }
 
 cargarProductos(productosArray);
@@ -170,7 +225,6 @@ botonesMarca.forEach(boton => {
   boton.addEventListener("click", (e) => {
     botonesMarca.forEach(b => b.classList.remove("active"));
     e.currentTarget.classList.add("active");
-
     const categoriaSeleccionada = e.currentTarget.id;
 
     const productosBoton = productosArray.filter(producto => {
@@ -196,6 +250,7 @@ contenedorProductos.addEventListener('click', (event) => {
         const productoId = event.target.id;
         agregarAlCarrito(productoId);
         actualizarCarrito();
+        actualizarPreciosCarrito()
         
     }
 });
@@ -209,11 +264,11 @@ carrito.addEventListener('click', (event) => {
   }
 });
 
-let tipoDeCambio = 1.2;
-fetch("https://api.exchangeratesapi.io/latest?base=USD&symbols=ARS")
+let tipoDeCambio = 1;
+fetch("https://dolarapi.com/v1/dolares/blue")
   .then(response => response.json())
   .then(data => {
-    tipoDeCambio = data.rates.ARS;
+    tipoDeCambio = data.compra;
     console.log("Tipo de cambio: " + tipoDeCambio);
     actualizarPrecios();
   })
@@ -225,16 +280,31 @@ function convertirPrecio(precio) {
   return precio * tipoDeCambio;
 }
 
-function mostrarPrecio(producto) {
-  let precio = convertirPrecio(producto.precio);
+function mostrarPrecio(producto, precio = null) {
+  if (precio === null) {
+    precio = convertirPrecio(producto.precio);
+  }
   let elemento = document.getElementById(producto.id);
   elemento.textContent = precio.toLocaleString("es-AR", { style: "currency", currency: "ARS" });
 }
+
 
 function actualizarPrecios() {
   productosArray.forEach(producto => {
     mostrarPrecio(producto);
   });
 }
+function actualizarPreciosCarrito() {
+  productosEnCarrito.forEach(producto => {
+    producto.precio = convertirPrecio(producto.precio);
+  });
+  actualizarCarrito();
+}
 
-setInterval(actualizarPrecios, 5000);
+
+setInterval(
+  actualizarPrecios
+, 5000);
+cargarProductosDesdeLocalStorage();
+actualizarCarrito();
+
